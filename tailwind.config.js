9b1a3d03b33d9118bb0ed69c2fcd83137b2552d4
andy_wendy <alexkens93@gmail.com>
2025-09-03 09:52:44 +0200

    installing tailwind cli and restructuring

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"], // adjust paths
+  theme: {
+    extend: {
+      fontFamily: {
+        foundation: ["foundation", "sans-serif"],
+      },
+      colors: {
+        primary: "#99d5c9",
+        secondary: "#6c969d",
+        tertiary: "#645E9D",
+        fontPrimary: "#392B58",
+        fontSecondary: "#2D0320",
+      },
+    },
+  },
+  plugins: [],
+};
