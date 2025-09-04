c50f8b359aa6cee5f107ca4e41dd3af54b5d66b7
andy_wendy <alexkens93@gmail.com>
2025-09-04 22:17:59 +0200

    nav links, finish profile cards, create github cards

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+const el = document.querySelector("div");
+
+const observer = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
+    el.classList.add("visible");
+  } else {
+    el.classList.remove("visible");
+  }
+});
+
+observer.observe(el);
+*/
