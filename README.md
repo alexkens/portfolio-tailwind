fa3101eb56039426d32c261b837c0cea0b571d0b
andy_wendy <alexkens93@gmail.com>
2025-09-07 15:02:48 +0200

    intersectino observer for profile section

---
+++ README.md
@@ -0,0 +1,2 @@
+build css with pnx:
+npx @tailwindcss/cli -i css/home.css -o dist/home.css --watch
--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-/*
-const el = document.querySelector("div");
+let options = {
+  root: null, // html element with a higher specificity than using "html"
+  rootMargin: "0px",
+  scrollMargin: "0px",
+  threshold: 0.01,
+};
 
-const observer = new IntersectionObserver(([entry]) => {
-  if (entry.isIntersecting) {
-    el.classList.add("visible");
-  } else {
-    el.classList.remove("visible");
-  }
+let profileLeft = document.querySelectorAll("#profile div #left");
+let profileRight = document.querySelectorAll('#profile div #right');
+
+// left
+const observerLeft = new IntersectionObserver(handleIntersectLeft, options);
+profileLeft.forEach(div => {
+  observerLeft.observe(div);
 });
 
-observer.observe(el);
-*/
+function handleIntersectLeft(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-comeFromLeft");
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+// right
+const observerRight = new IntersectionObserver(handleIntersectRight, options);
+profileRight.forEach(div => {
+  observerRight.observe(div);
+});
+
+function handleIntersectRight(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-comeFromRight");
+      observer.unobserve(entry.target);
+    }
+  });
+}
