301ed3ac8ccae5c6f360ad7980aae204e3c3adde
andy_wendy <alexkens93@gmail.com>
2025-09-09 22:21:01 +0200

    darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,15 @@
-document.documentElement.classList.toggle(
-  "dark",
-  localStorage.theme === "dark" ||
-    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-);
+
+function darkmode() {
+
+    const body = document.body;
+    const darkmodeButton = document.getElementById("darkmodeButton");
+
+    if(body.classList.contains("dark")){
+        body.classList.remove("dark");
+        darkmodeButton.innerHTML = "Darkmode";
+
+    } else {
+        body.classList.add("dark");
+        darkmodeButton.innerHTML = "Lightmode";
+    }
+}
